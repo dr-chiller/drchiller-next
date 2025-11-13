0a8f6e087d12288093199a9251fad183327cd521
Melbin Mathew <melbinppmathewp@gmail.com>
2025-11-13 21:28:51 +0530

    20th commit - Added google tag scripts in footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,6 +3,7 @@
 import { BiMobile } from "react-icons/bi";
 import { MdEmail, MdLocationOn, MdPhone } from "react-icons/md";
 import Link from "next/link";
+import Script from "next/script";
 
 const Footer = () => {
     return (
@@ -141,6 +142,26 @@ const Footer = () => {
                     </a>
                 </span>
             </div>
+
+            {/* Google Tag Manager Script */}
+            <Script id="gtm-footer" strategy="afterInteractive">
+                {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MTZL8HSW');
+        `}
+            </Script>
+
+            <noscript>
+                <iframe
+                    src="https://www.googletagmanager.com/ns.html?id=GTM-MTZL8HSW"
+                    height="0"
+                    width="0"
+                    style={{ display: "none", visibility: "hidden" }}
+                ></iframe>
+            </noscript>
         </footer>
     );
 };
